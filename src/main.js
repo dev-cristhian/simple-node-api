60abacb8993d9924a791a23ff929fd70e3a23bc7
Cristhian Santos <cristhianfelipe0@gmail.com>
2025-07-22 18:39:39 -0300

    ✨ feat: implement startup project and configure initial settings

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import http from "http";
+
+import { ROUTER } from "./routes.js";
+import { notFound } from "./utils/http.util.js";
+
+const server = http.createServer(async (request, response) => {
+  const { url, method } = request;
+
+  const routeData = ROUTER.getRouteDate(url, method);
+
+  if (!routeData) {
+    notFound(response, { message: `Cannot ${method} / ${url}` });
+    return;
+  }
+
+  const { handler } = routeData;
+  await handler(request, response);
+});
+
+server.listen(3000, () => {
+  console.info(`🔥 server is running on http://localhost:3000`);
+});
